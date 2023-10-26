1254fa3f2ce3a710996ca80ef9ebe028d72f6ccf
LabA1_PC13 <harsordhuaylla@gmail.com>
2023-10-26 11:24:05 -0400

    Ejercicio 3.6 Finalizado

---
+++ src/Ejercicio3_2/Cadena.java
@@ -0,0 +1,5 @@
+package Ejercicio3_2;
+
+public class Cadena {
+    String cadenaInicial = " Programacion Orientada a Objetos ";
+}
---
+++ src/Ejercicio3_6/Estudiante.java
@@ -0,0 +1,18 @@
+package Ejercicio3_6;
+
+public class Estudiante {
+    String nombre;
+    String apellido;
+    int codigo;
+    int semestre;
+    double notaFinal;
+
+
+    public Estudiante(String nombre,String apellido,int codigo,int semestre,double notaFinal){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.codigo=codigo;
+        this.semestre=semestre;
+        this.notaFinal=notaFinal;
+    }
+}
